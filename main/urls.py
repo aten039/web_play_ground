114c9607f4ac079b1820b0a76c7b509cb3f084ed
aten039 <l.elvis039@gmail.com>
2024-12-14 18:31:16 -0400

    register with email, extends forms

--- main/urls.py
+++ main/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path of Auth
     path("accounts/", include('django.contrib.auth.urls')),
+    path("accounts/", include('registration.urls')),
 
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LogoutView 
+from .forms import UserCreationFormWithEmail
+from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django import forms 
 
 # Create your views here.
+
+class SignUpView(CreateView):
+    form_class = UserCreationFormWithEmail
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+    def get_success_url(self):
+        return reverse('login') + '?register'
+    
+    def get_form(self, form_class = None):
+        form = super(SignUpView, self).get_form()
+        
+        form.fields['username'].widget = forms.TextInput(attrs={"class": "form-control mb-2", "placeholder": "Nombre de usuario"})
+        form.fields['email'].widget = forms.EmailInput(attrs={"class": "form-control mb-2", "placeholder": "E-mail"})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={"class": "form-control mb-2", "placeholder": "contraseña"})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={"class": "form-control mb-2", "placeholder": "confirma tu contraseña"})
+
+        return form
+
